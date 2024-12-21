8814e9c4fe660889168af7ef58833709b415735c
Gededito <dito999dito@gmail.com>
2024-12-21 12:36:36 +0700

    perbarui

--- lib/core/constants/constant.dart
+++ lib/core/constants/constant.dart
@@ -1,5 +1,5 @@
 class Variables {
   static const String baseUrl = "http://192.168.0.171:8000";
-  static const String googelApi = "";
+  static const String googelApi = "AIzaSyBDBVqza0lirpNq1ewLwmkVOvpUPq2TD1k";
   static const String apiKeyMap = '91d5f6d7ed6c5a7e93de90979a0a5ff4';
 }
